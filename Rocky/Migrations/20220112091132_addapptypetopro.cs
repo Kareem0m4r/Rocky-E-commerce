9d548e07c9bf89336088494bc31ee293a08735b0
Kareem Omar <k4r33m0m4r@gmail.com>
2023-05-27 13:33:06 +0300

    initial commit

---
+++ Rocky/Migrations/20220112091132_addapptypetopro.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky.Migrations
+{
+    public partial class addapptypetopro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppTypeId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_AppTypeId",
+                table: "Product",
+                column: "AppTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ApplicationType_AppTypeId",
+                table: "Product",
+                column: "AppTypeId",
+                principalTable: "ApplicationType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ApplicationType_AppTypeId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_AppTypeId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "AppTypeId",
+                table: "Product");
+        }
+    }
+}
---
+++ Rocky/Migrations/20220112093927_AddApplicationTypeToProd.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rocky.Migrations
+{
+    public partial class AddApplicationTypeToProd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ApplicationType_AppTypeId",
+                table: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "AppTypeId",
+                table: "Product",
+                newName: "ApplicationTypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_AppTypeId",
+                table: "Product",
+                newName: "IX_Product_ApplicationTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ApplicationType_ApplicationTypeId",
+                table: "Product",
+                column: "ApplicationTypeId",
+                principalTable: "ApplicationType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ApplicationType_ApplicationTypeId",
+                table: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationTypeId",
+                table: "Product",
+                newName: "AppTypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_ApplicationTypeId",
+                table: "Product",
+                newName: "IX_Product_AppTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ApplicationType_AppTypeId",
+                table: "Product",
+                column: "AppTypeId",
+                principalTable: "ApplicationType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
